ce59d9f3dc0ab01acdd4a67af26ac1c7af72cd10
Nelson <nelson.villegas@me.com>
2022-12-05 15:29:35 -0700

    correccion bug filtro

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -4,15 +4,38 @@ const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto, filtro, gastosFi
 
   return (
     <div className="listado-gastos contenedor">
-        <h2>{ gastos.length ? 'Gastos':'No hay gastos aun' }</h2>
-        {gastos.map( gasto => (
-            <Gasto
-                key={gasto.id}
-                gasto={gasto}
-                setGastoEditar={ setGastoEditar }
-                eliminarGasto={ eliminarGasto }
-            />
-        ))}
+        
+        {
+          filtro ? (
+            <>
+            <h2>{ gastosFiltrados.length ? 'Gastos':'No hay gastos en esta categoria' }</h2>
+            {
+              gastosFiltrados.map( gasto => (
+                <Gasto
+                    key={gasto.id}
+                    gasto={gasto}
+                    setGastoEditar={ setGastoEditar }
+                    eliminarGasto={ eliminarGasto }
+                />
+              ))
+            }</>
+          )
+          :
+          (
+            <>
+            <h2>{ gastos.length ? 'Gastos':'No hay gastos aun' }</h2>
+            {
+              gastos.map( gasto => (
+                <Gasto
+                    key={gasto.id}
+                    gasto={gasto}
+                    setGastoEditar={ setGastoEditar }
+                    eliminarGasto={ eliminarGasto }
+                />
+              ))
+            }</>
+          )
+        }
     </div>
   )
 }
